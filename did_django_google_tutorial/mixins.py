e6d0ccdc5c3e7dd6256432b8b8b937c972b28364
Davi Castro <davialmeida47jd@gmail.com>
2022-08-28 23:22:18 -0300

    feat(mixins): created function to append url parameters when redirecting users

--- did_django_google_tutorial/mixins.py
+++ did_django_google_tutorial/mixins.py
@@ -31,3 +31,16 @@ def reCAPTCHAValidation(token):
         })
     
     return result.json()
+
+
+def RedirectParams(**kwargs):
+    '''
+    Used to append url parameters when redirecting users
+    '''
+    url = kwargs.get('url')
+    params = kwargs.get('params')
+    response = redirect(url)
+    if params:
+        query_string = urlencode(params)
+        response['Location'] += '?' + query_string
+    return response
