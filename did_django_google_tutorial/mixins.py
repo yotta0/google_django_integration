6854aaa78c7265ea689903913514de1e468c806b
Davi Castro <davialmeida47jd@gmail.com>
2022-08-28 23:51:20 -0300

    feat(mixins): created function to handle Directions google api requests

--- did_django_google_tutorial/mixins.py
+++ did_django_google_tutorial/mixins.py
@@ -48,7 +48,7 @@ def RedirectParams(**kwargs):
 
 class AjaxFormMixin(object):
     '''
-    Mixin to ajaxify django form - can be over written in view by calling 
+    Mixin to ajaxify django form - can be over written in view
     '''
 
     def form_invalid(self, form):
@@ -64,3 +64,74 @@ class AjaxFormMixin(object):
             form.save()
             return JsonResponse({'result': 'Success', 'message': ''})
         return response
+
+
+def Directions(*args, **kwargs):
+	'''
+	Handles directions from Google
+	'''
+
+	lat_a = kwargs.get("lat_a")
+	long_a = kwargs.get("long_a")
+	lat_b = kwargs.get("lat_b")
+	long_b = kwargs.get("long_b")
+	lat_c = kwargs.get("lat_c")
+	long_c = kwargs.get("long_c")
+	lat_d = kwargs.get("lat_d")
+	long_d = kwargs.get("long_d")
+
+	origin = f'{lat_a},{long_a}'
+	destination = f'{lat_b},{long_b}'
+	waypoints = f'{lat_c},{long_c}|{lat_d},{long_d}'
+
+	result = requests.get(
+		'https://maps.googleapis.com/maps/api/directions/json?',
+		 params={
+		 'origin': origin,
+		 'destination': destination,
+		 'waypoints': waypoints,
+		 "key": settings.GOOGLE_API_KEY
+		 })
+
+	directions = result.json()
+
+	if directions["status"] == "OK":
+
+		routes = directions["routes"][0]["legs"]
+
+		distance = 0
+		duration = 0
+		route_list = []
+
+		for route in range(len(routes)):
+
+			distance += int(routes[route]["distance"]["value"])
+			duration += int(routes[route]["duration"]["value"])
+
+			route_step = {
+				'origin': routes[route]["start_address"],
+				'destination': routes[route]["end_address"],
+				'distance': routes[route]["distance"]["text"],
+				'duration': routes[route]["duration"]["text"],
+
+				'steps': [
+					[
+						s["distance"]["text"],
+						s["duration"]["text"],
+						s["html_instructions"],
+
+					]
+					for s in routes[route]["steps"]]
+				}
+
+			
+			route_list.append(route_step)
+			
+
+	return {
+		"origin": origin,
+		"destination": destination,
+		"distance": f"{round(distance/1000, 2)} Km",
+		"duration": format_timespan(duration),
+		"route": route_list
+		}
