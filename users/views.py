1492ecc6f2db31ff3ff6dda13471f3f6473047fd
Davi Castro <davialmeida47jd@gmail.com>
2022-10-09 23:50:40 -0300

    feat(views): created users views and Account Template View class

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import Login, Logout, authenticate
+from django.conf import settings
+from django.http import JsonResponse
+from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+from did_django_google_tutorial.mixins import(
+    AjaxFormMixin,
+    reCAPTCHAValidation,
+    FormErrors,
+    RedirectParams,
+    )
+
+from .forms import (
+    UserForm,
+    UserProfileForm,
+    AuthForm,
+)
+
+
+class AccountView(TemplateView):
+    '''
+    Generic FormView with our mixin to display user account page
+    '''
+    template_name = 'users/account.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
